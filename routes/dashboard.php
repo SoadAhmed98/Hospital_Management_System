607891cb553c003cf71107a9370e088ccd1500d5
Fatma Elzahraa Atef <88749492+fatma8720@users.noreply.github.com>
2024-06-07 12:44:43 +0300

    rtl and ltr

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,5 +4,14 @@ use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
  
-Route::resource('backend', DashboardController::class);
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+        require __DIR__.'/auth.php';
+        Route::resource('backend', DashboardController::class);
+
+    });
 
