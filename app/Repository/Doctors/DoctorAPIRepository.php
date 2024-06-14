40156e595d6cbd3ee73f1295dc5e035797e770d3
Soad Ahmed <soadahmed1498@gmail.com>
2024-06-14 17:06:49 +0300

    make doctors api for all doctors and doctor details

--- app/Repository/Doctors/DoctorAPIRepository.php
+++ app/Repository/Doctors/DoctorAPIRepository.php
@@ -1,29 +1,31 @@
 <?php
 
-namespace App\Repository\Departments;
+namespace App\Repository\Doctors;
 
 use App\Models\Doctor;
 use Illuminate\Support\Facades\Hash;
-use App\Interfaces\Departments\DoctorAPIRepositoryInterface;
+use App\Interfaces\Doctors\DoctorAPIRepositoryInterface;
 
 class DoctorAPIRepository implements DoctorAPIRepositoryInterface
-{public function index()
+{
+    public function index()
     {
-        return Doctor::all();
+        return Doctor::with('image')->get();
     }
 
     public function show($id)
     {
-        return Doctor::findOrFail($id);
+        return Doctor::with('image')->findOrFail($id);
+       
     }
 
-    public function store($request)
-    {
-        return Doctor::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('description'),
-        ]);
-    }
+    // public function store($request)
+    // {
+    //     return Doctor::create([
+    //         'name' => $request->input('name'),
+    //         'email' => $request->input('description'),
+    //     ]);
+    // }
 
 
 }
