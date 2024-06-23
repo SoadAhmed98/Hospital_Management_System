50052c7516b99aab9260aecf6048f70a5402707a
Soad Ahmed <soadahmed1498@gmail.com>
2024-06-23 14:31:16 +0300

    edit doctor api and make resource for it

--- app/Repository/Doctors/DoctorAPIRepository.php
+++ app/Repository/Doctors/DoctorAPIRepository.php
@@ -11,45 +11,15 @@ class DoctorAPIRepository implements DoctorAPIRepositoryInterface
     public function index()
     {
       
-        $doctors = Doctor::with('image')->get();
-
-        return   $doctorsData = $doctors->map(function($doctor) {
-            $doctorData = $doctor->toArray();
-            $image = $doctor->image;
-            if ($image) {
-                $doctorData['image'] = "http://127.0.0.1:80/Dashboard/img/doctors/".$image->filename; 
-            } else {
-                $doctorData['image'] = null;
-            }
-
-            return $doctorData;
-        });
-       
+        return Doctor::with('image')->where('status', 1)->get();
+   
     }
 
     public function show($id)
     {
-        $doctor = Doctor::with('image')->findOrFail($id);
+        return Doctor::with('image')->where('status', 1)->findOrFail($id);
 
-        $doctorWithimage = $doctor->toArray();
-    
-        if ($doctor->image) {
-            $doctorWithimage['image'] = "http://127.0.0.1:80/Dashboard/img/doctors/" . $doctor->image->filename;
-        } else {
-            $doctorWithimage['image'] = null;
-        }
-
-        return $doctorWithimage;
-       
     }
-
-    // public function store($request)
-    // {
-    //     return Doctor::create([
-    //         'name' => $request->input('name'),
-    //         'email' => $request->input('description'),
-    //     ]);
-    // }
 
 
 }
