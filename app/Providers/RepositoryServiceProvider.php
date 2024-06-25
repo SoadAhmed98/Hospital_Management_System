656eb1a4c09d266298431a02ce2086da67a96ea0
Fatma Elzahraa Atef <88749492+fatma8720@users.noreply.github.com>
2024-06-25 18:06:50 +0300

    Invoices Before Doctor Dashboard

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -36,6 +36,9 @@ use App\Interfaces\Services\SingleServiceRepositoryInterface;
 use App\Repository\Services\SingleServiceRepository;
 
 
+use App\Interfaces\doctor_dashboard\InvoicesRepositoryInterface;
+use App\Repository\doctor_dashboard\InvoicesRepository;
+
 use App\Interfaces\doctor_dashboard\LaboratoriesRepositoryInterface;
 use App\Repository\doctor_dashboard\LaboratoriesRepository;
 
@@ -62,6 +65,10 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(DoctorRepositoryInterface::class, DoctorRepository::class);
         $this->app->bind(DoctorAPIRepositoryInterface::class, DoctorAPIRepository::class);
         $this->app->bind(SingleServiceRepositoryInterface::class, SingleServiceRepository::class);
+
+
+        // doctor
+        $this->app->bind(InvoicesRepositoryInterface::class, InvoicesRepository::class);
 
         $this->app->bind(LaboratoriesRepositoryInterface::class, LaboratoriesRepository::class);
 
